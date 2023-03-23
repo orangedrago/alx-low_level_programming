dc109903f9e7b7048bb4ff012056321c27aa8930
orangedragon <odgarba@gmail.com>
2023-03-23 11:42:37 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a diagonal line in the terminal
- * @n: the number of times the character '\' should be printed
+ * print_square - Prints a square in the terminal
+ * @size: Size of the square
+ *
+ * Return: void
  */
-void print_diagonal(int n)
+
+void print_square(int size)
 {
 int i, j;
-if (n <= 0)
+if (size <= 0)
 {
 _putchar('\n');
 return;
 }
-for (i = 0; i < n; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; j < i; j++)
-_putchar(' ');
-_putchar('\\');
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
 _putchar('\n');
 }
 }
